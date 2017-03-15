6ef8a50bdd7c0859713e1e4594a1f445a6f2064f
Alex Sotiriou <a.sotiriou@difio.gr>
2017-03-15 20:28:51 +0200

    Fixed upload_path utility function.

--- groundworks/utils.py
+++ groundworks/utils.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth import get_user_model
+from django.utils.encoding import force_text
 
 
 def has_edit_user_permissions(user):
@@ -19,11 +20,11 @@ def has_edit_user_permissions(user):
 def upload_path(instance, filename):
     """
     A callable for creating upload paths for files. The output path will be
-    <app_name>/<model>/<instance.slug or instance.pk>/
+    <app_label>/<model>/<instance.pk>/filename
 
     See: https://docs.djangoproject.com/en/1.10/ref/models/fields/#django.db.models.FileField.upload_to
     """
-    app_name = instance._meta.app_name
-    model = instance._meta.model.lower()
-    identifier = instance.slug if hasattr(instance, slug) else instance.pk
-    return '/'.join([app_name, model, identifier, ''])
+    app_name = instance._meta.app_label
+    model = instance._meta.model.__name__.lower()
+    identifier = force_text(instance.pk)
+    return '/'.join([app_name, model, identifier, filename])
