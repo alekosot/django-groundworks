47019407a30475c8ff45692d19c22f69c3a3e382
Alex Sotiriou <a.sotiriou@difio.gr>
2017-04-01 16:18:39 +0300

    Added sorted_languages_info template tag.

--- groundworks/templatetags/i18n_utils.py
+++ groundworks/templatetags/i18n_utils.py
@@ -4,6 +4,11 @@
 from __future__ import unicode_literals
 
 from django import template
+from django.conf import settings
+from django.utils.translation import (
+    get_language_from_request,
+    get_language_info
+)
 
 
 register = template.Library()
@@ -22,3 +27,30 @@ def naive_i18n_url(context, lang):
     path_parts = context['request'].path.split('/')
     path_parts[1] = lang
     return '/'.join(path_parts)
+
+
+@register.simple_tag(takes_context=True)
+def sorted_languages_info(context, check_path=True):
+    """
+    Wrapper around django's ``get_language_info`` i18n utility, that returns
+    the "info" for all languages given in settings.LANGUAGES, with the
+    active one being first. The identification of the current language is done
+    with the use of ``get_language_from_request``, which is passed the
+    ``check_path`` parameter.
+
+    Usage::
+
+        {% sorted_languages_info as langs %}
+        {% for l in langs %}
+            {{ l.code }}
+            {{ l.name }}
+            {{ l.name_translated }}
+            {{ l.name_local }}
+            {{ l.bidi|yesno:"bi-directional,uni-directional" }}
+        {% endfor %}
+    """
+    langs = [_lang[0] for _lang in settings.LANGUAGES]
+    current_lang = get_language_from_request(context['request'], check_path)
+    langs.remove(current_lang)
+    langs.insert(0, current_lang)
+    return [get_language_info(lang) for lang in langs]
